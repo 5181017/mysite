4f12cc583ff97d8f2c793f9e52e3ea78ef6f5fe4
5181017 <5181017@st.hsc.ac.jp>
2020-08-19 15:51:57 +0900

    8/19_2

--- polls/views/Class/Cart.py
+++ polls/views/Class/Cart.py
@@ -1,3 +1,4 @@
+from polls import models
 from polls.views.Class.Product import Product
 
 
@@ -6,7 +7,17 @@ class Cart:
     quantity = 0  # 個数
 
     # カートの削除
+    def delete_cart(self, userid, productid):
+        data = models.Cart.objects.filter(userID=userid, productID=productid)
+        data.delete()  # DELETEが実行される
+
     # カートの追加
+    def insert_cart(self, userid, productid, quantity):
+        data = models.Cart(userID=userid, productID=productid, quantity=quantity)
+        data.save()  # INSERTが実行される
+
     # カートの更新
-    def cart(self):
-        print("cart")
+    def updata_cart(self, userid, productid, quantity):
+        data = models.Cart.objects.get(userID=userid, productID=productid)
+        data.quantity = quantity
+        data.save()  # ここでUPDATEが実行される

3b77e61e9ac3e66148836bca8d0418833ec33844
5181026 <5181026@st.hsc.ac.jp>
2020-08-26 13:21:27 +0900

    HTMLのテスト、Exception 2

--- polls/views/Class/User.py
+++ polls/views/Class/User.py
@@ -15,13 +15,21 @@ class User:
         if(user.exists()):
             return user
 
-        return ValueError
+        raise models.User.DoesNotExist
 
     # ユーザの登録
-    def register_user(self , userId , userName , userPass):
-        user = models.User.objects.filter(userID=self.userId)
+    def register_user(self, userID, name, pw):
+        user = models.User.objects.filter(userID=self.userID)
+
+        if user.exists():
+            return "すでに存在します"
+
+        register = models.User(userID=userID, name=name, pw=pw, money=self.remaining_money, address=self)
+        models.User.save(register)
         
     # ユーザの更新
+    def update_user(self, money, address, cart):
+        print("hello")
     # ユーザの削除
     # ユーザの取得
     # 残高のチャージ

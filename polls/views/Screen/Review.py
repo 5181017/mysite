9d5950322a54ac252709f96947e33c81c388865c
5181017 <5181017@st.hsc.ac.jp>
2020-09-16 13:09:27 +0900

    9/16-1

--- polls/views/Screen/Review.py
+++ polls/views/Screen/Review.py
@@ -4,16 +4,15 @@ from polls.views.Class.Product import Product
 from polls.views.Class.Review import Review
 
 
-def review(request , product_id):
-    #TODO
-    # 画像を載せる
+def review(request, product_id):
+    # TODO画像を載せる
     if request.method == "GET":
         # product_id = request.GET.get("product_id")
         product = Product().get_product(product_id)
 
         params = {
-            "product_name" : product.productName,
-            "product_img" : product.imageURL,
+            "product_name": product.productName,
+            "product_img": product.imageURL,
         }
         return render(request, 'polls/review.html', params)
     elif request.method == "POST":
@@ -21,6 +20,6 @@ def review(request , product_id):
         reviewstar = request.POST["reviewstar"]
         title = request.POST["title"]
         comment = request.POST["commnet"]
-        user_id = request.session['user']        #useridだけをとる
-        rv.register_review(user_id , reviewstar , title , comment)
+        user_id = request.session['user']  # useridだけをとる
+        rv.register_review(user_id, reviewstar, title, comment)
         return redirect("/product/" + product_id)
